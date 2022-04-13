ce7e76385bc014c44f5f8bb0916016e2e42269c4
Whitsel <33960586+Whitsel@users.noreply.github.com>
2022-04-13 00:05:35 -0800

    Added cfgOption for jump

--- Static_Line.VR/tScripts/StaticLine/cfgStaticLine.hpp
+++ Static_Line.VR/tScripts/StaticLine/cfgStaticLine.hpp
@@ -1,4 +1,15 @@
 
 //Immersion Settings
 
-#define playerRetrieveBag 1
+class CfgStaticLine {
+	enablePlayerBagRetrieval = 1;
+	enablePlayerWeaponRetrieval = 1;
+	enablePlayerJumpRefusals = 1;
+	enablePlayerManualEquipmentLowering = 1;
+	enablePlayerEquipmentJetison = 1;
+
+	enableLoweredEquipmentChemlights = 1;
+	enableInjuredChemlightMinor = 1;
+	enableInjuredChemlightMajor = 1;
+	enableDamageOnEquipmentRideIn = 1;
+};
